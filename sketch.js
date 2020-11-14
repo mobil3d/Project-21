990ff5a022514613395ddd65c38007f812e84d39
mobil3d <72637100+mobil3d@users.noreply.github.com>
2020-11-14 15:36:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,26 +23,7 @@ function setup() {
 function draw() {
   background(0,0,0);  
 
-  if(wall.x -bullet.x < (bullet.width + wall;;.width/2){
-  
-  bullet.velocityX = 0;
-  var deformation = 0.5 *weight*speed*speed/22509; 
-  if(deformation>180)
-  {
-    bullet.shapeColor = color(255,0,0);
-  }
-
-  if (deformation<180 && deformation>100)
-  {
-    bullet.shapeColor = color(230,230,0);
-  }
-
-if(deformation<100)
-{
-  bullet.shapeColor = color(0,255,0);
-}
-
- }
+ 
 
 
 
@@ -69,8 +50,8 @@ if(hasCollided(bullet,wall)){
 
 
 function hasCollided(Lbullet, Lwall){
-    bulletRightEdge = lbullet.x + 1bullet.width;
-    wallLeftEdge = 1wall.x;
+    Lbullet = lbullet.x + Lbullet.width;
+    Lwall = Lwall.x;
 
   if(bulletRightEdge>=wallLeftEdge){
     return true
