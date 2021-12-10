4c4e3318ed4f9d0802e3a3868458d1ba8201aa50
mobil3d <72637100+mobil3d@users.noreply.github.com>
2021-12-09 22:07:12 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,45 @@
-var wall,thickness;
-var bullet,speed,weight;
+var car, wall;
+var speed,weight;
 
 
 function setup() {
   createCanvas(1600,400);
-  
-  thickness = random(22,83);
 
-  speed = random(223,321);
-  weight = random(30,52);
+  speed = random(55,90);
+  weight = random(400, 1500);
 
-  bullet = createSprite(50, 200, 20, 20);
-  bullet.velocityX = speed;
-  bullet.shapeColor = color(355,355,355);
+  car = createSprite(50, 200, 20, 20);
 
-  wall = createSprite(1200,200,thickness,height/2);
+ 
+  car.velocityX = speed;
+  car.shapeColor = color(255);
+
+  wall = createSprite(1500,200, 60, height/2);
   wall.shapeColor = color(80,80,80); 
-  
-  
+ 
 }
 
 function draw() {
   background(0,0,0);  
 
- 
+
+if(wall.x-car.x < (car.width+wall.width)/2){
+
+  car.velocityX = 0;
+  var deformation = 0.5 * weight * speed/22500;
 
 
-
-if(hasCollided(bullet,wall)){
-
-  bullet.velocityX = 0;
-  var damage = 0.5 * weight * speed/(thickness * thickness);
-
-
-  if(damage>10){
-      wall.shapeColor = color(255,0,0);
+  if(deformation<180 && deformation>80){
+      car.shapeColor = color(255,255,0);
   }
 
 
-  if(damage<10){
-    wall.shapeColor = color(0,255,0);
+  if(deformation<80){
+    car.shapeColor = color(0,255,0);
+  }
+
+  if(deformation>180){
+    car.shapeColor = color(255,0,0);
   }
 }
 
@@ -49,13 +49,4 @@ if(hasCollided(bullet,wall)){
   }
 
 
-function hasCollided(Lbullet, Lwall){
-    Lbullet = lbullet.x + Lbullet.width;
-    Lwall = Lwall.x;
-
-  if(bulletRightEdge>=wallLeftEdge){
-    return true
-  }  
-return false;
-}
 
